663e16b00ff0c0004b34e74b79a64e582d8b5df2
rcmempresa <rcmiranda.empresa@gmail.com>
2025-06-16 18:54:01 +0100

    mais alteracoes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -614,7 +614,7 @@ const rentalMachines = [
     <motion.section
       initial="hidden"
       whileInView="visible"
-      viewport={{ once: true, amount: 0.2 }}
+      viewport={{ once: true, amount: 0.1 }}
       variants={slideInLeft}
       className="py-16 lg:py-24 bg-white"
     >
